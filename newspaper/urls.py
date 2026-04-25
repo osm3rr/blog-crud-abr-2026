6b95cf86e132fd08c7491c82897d18b0a50b5c5c
osm3rr <osm3rr@aakademy.com>
2026-04-25 11:37:16 -0400

    article detail done

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list'),
+    path('', ArticleListView.as_view(), name='article-list'),
+    path('articulo/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
 ]
