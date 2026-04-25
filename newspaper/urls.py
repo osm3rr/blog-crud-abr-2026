78e320fce3d1f96f6fde54f0c5d77aa1051bc9f5
osm3rr <osm3rr@aakademy.com>
2026-04-25 12:15:11 -0400

    article create done

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import (ArticleListView,
+                    ArticleDetailView,
+                    ArticleCreateView,
+                    )
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
     path('articulo/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('articulo/crear/', ArticleCreateView.as_view(), name='article-create'),
 ]
